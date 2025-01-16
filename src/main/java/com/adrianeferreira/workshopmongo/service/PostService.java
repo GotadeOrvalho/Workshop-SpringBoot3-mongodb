d78df4b2f39e39c8e089da6e3669ec1afc15c83f
Adriane Ferreira <adrianeferreirap@gmail.com>
2025-01-16 15:22:08 -0300

    Obtendo um post por id

---
+++ src/main/java/com/adrianeferreira/workshopmongo/service/PostService.java
@@ -0,0 +1,29 @@
+package com.adrianeferreira.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adrianeferreira.workshopmongo.domain.Post;
+import com.adrianeferreira.workshopmongo.repository.PostRepository;
+import com.adrianeferreira.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	
+	
+	public Post findById(String id) {
+		
+		
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+	
+}
